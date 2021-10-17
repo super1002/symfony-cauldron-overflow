27b81a7f07e464ca20f5365e6557639d9ebdb1e8
super1002 <flexiway.studio@gmail.com>
2021-10-17 22:22:01 +0300

    added cache to QuestionController.php

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -30,7 +31,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser)
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
@@ -38,8 +39,10 @@ class QuestionController extends AbstractController
             'Maybe... try saying the spell backwards?',
         ];
 
-        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $cache->get('md_'.md5($questionText), function() use ($questionText, $markdownParser) {
+            return $markdownParser->transformMarkdown($questionText);
+        });
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
